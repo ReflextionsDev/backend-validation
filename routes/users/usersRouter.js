1eae5cc39a6eb9f8be2569fd58899a93651bb029
ReflextionsDev <reflextionsdev@gmail.com>
2022-02-23 15:53:14 -0500

    index middleware

--- routes/users/usersRouter.js
+++ routes/users/usersRouter.js
@@ -1,20 +1,19 @@
 var express = require('express');
 var router = express.Router();
 const { createUser, userLogin } = require('./controller/userController')
-const { checkIsEmpty } = require('../lib/validationMiddleware/checkIsEmpty');
-const { validateCreateData } = require('../lib/validationMiddleware/validateCreateData');
-const { validateEmail } = require('../lib/validationMiddleware/validateEmail');
-const { jwtMiddleware } = require('../lib/validationMiddleware/jwtMiddleware');
-const { validateUpdateData } = require('../lib/validationMiddleware/validateUpdateData');
+const { checkIsEmpty,
+  jwtMiddleware,
+  validateCreateData,
+  validateEmail,
+  validateUpdateData } = require('../lib/validationMiddleware/index')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
 router.post('/create-user', checkIsEmpty, validateCreateData, createUser)
 router.post('/login', validateEmail, userLogin)
 router.post('/update-profile', jwtMiddleware, checkIsEmpty, validateUpdateData)
-
 
 module.exports = router
