58c7a7dd4df8d424cd2448d5a987ebd7cbb7dab7
ReflextionsDev <reflextionsdev@gmail.com>
2022-02-23 15:46:10 -0500

    Add password update

---
+++ routes/lib/validationMiddleware/jwtMiddleware.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken')
+
+const jwtMiddleware = async (req, res, next) => {
+
+    try {
+
+
+
+        if (req.headers && req.headers.authorization) {
+            let token = req.headers.authorization.slice(7)
+            const decodedToken = jwt.verify(token, process.env.SECRET_KEY)
+
+            console.log({ decodedToken })
+            // res.status(200).json({ decodedToken })
+            next()
+        } else {
+            throw { message: "You don't have permission" }
+        }
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+module.exports = {
+    jwtMiddleware,
+}
--- routes/users/usersRouter.js
+++ routes/users/usersRouter.js
@@ -4,6 +4,8 @@ const { createUser, userLogin } = require('./controller/userController')
 const { checkIsEmpty } = require('../lib/validationMiddleware/checkIsEmpty');
 const { validateCreateData } = require('../lib/validationMiddleware/validateCreateData');
 const { validateEmail } = require('../lib/validationMiddleware/validateEmail');
+const { jwtMiddleware } = require('../lib/validationMiddleware/jwtMiddleware');
+const { validateUpdateData } = require('../lib/validationMiddleware/validateUpdateData');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -12,5 +14,7 @@ router.get('/', function(req, res, next) {
 
 router.post('/create-user', checkIsEmpty, validateCreateData, createUser)
 router.post('/login', validateEmail, userLogin)
+router.post('/update-profile', jwtMiddleware, checkIsEmpty, validateUpdateData)
 
-module.exports = router;
+
+module.exports = router
